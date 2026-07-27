f1651253bdb687ba22cb2a7c6aef763fcf01a548
skyblockhaven <skyblockhaven@gmail.com>
2026-07-27 16:17:42 +0200

    Add functions to copy server IP to clipboard

--- index.php/js/servers.js
+++ index.php/js/servers.js
@@ -1,1 +1,14 @@
 
+const serverIP = "ender.skyblockhaven.com";
+
+function copyIP(){
+    navigator.clipboard.writeText(serverIP);
+    alert("Server IP copied: " + serverIP);
+}
+
+const serverIP2 = "";
+
+function copyIP2(){
+    navigator.clipboard.writeText(serverIP2);
+    alert("Server IP copied: " + serverIP2);
+}
